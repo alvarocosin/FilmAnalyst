b8f086283b91d0f5372fe1e3bfa337e09ee86ad0
alvarocosin <cosin.alvaro@gmail.com>
2022-12-06 12:09:03 +0100

    Added movie class

--- mispelis.py
+++ mispelis.py
@@ -1,28 +1,25 @@
 # Lee una lista de películas desde un .txt y los escribe en una hoja excel
 import pandas as pd
+from movie import Movie
 #Reads movie list file and creates a list from it
 with open('pelis2021.txt') as f:
     lines = f.readlines()
 f.close()
 
 movies = list()
-years = list()
-directors = list()
 
-#Separates data from the movie, year and director and makes a list from each one
+#Separates movie, year and director from data and makes a movie list
 for line in lines:
     line = line.strip()
     firstbreak = line.find('(')
-    movie = line[2:firstbreak - 1]
-    movies.append(movie)
+    title = line[2:firstbreak - 1]
     year = line[firstbreak + 1:firstbreak + 5]
-    years.append(year)
     secondbreak = line.find('.')
     director = line[secondbreak + 2:]
-    directors.append(director)
-
+    movie = Movie(title=title, year=year, director=director)
+    movies.append(movie)
 #Crea un csv con los datos 
-length = len(movies)
-df = pd.DataFrame({'Título': movies, 'Año': years, 'Director': directors}, index=list(range(1,length+1)))
-df.to_csv('Pelis2021.csv', index = False)
-print('Done')
+# length = len(movies)
+# df = pd.DataFrame({'Título': movies.title, 'Año': movies.year, 'Director': movies.director}, index=list(range(1,length+1)))
+# df.to_csv('Pelis2021.csv', index = False)
+# print('Done')
---
+++ movie.py
@@ -0,0 +1,6 @@
+class Movie():
+    
+    def __init__(self, title, year, director):
+        self.title = title
+        self.year = year
+        self.director = director
